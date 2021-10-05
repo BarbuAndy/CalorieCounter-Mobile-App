0d598e4146411e6a4f71fcbcd82c4ff528ebf259
BarbuAndy <barbu.a.v99@gmail.com>
2021-10-05 13:00:20 +0300

    meal added

---
+++ CC/CC/Migrations/20210817070714_v9.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CC.Migrations
+{
+    public partial class v9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "published",
+                table: "FoodItem",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "timesFlaggedWrong",
+                table: "FoodItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "published",
+                table: "FoodItem");
+
+            migrationBuilder.DropColumn(
+                name: "timesFlaggedWrong",
+                table: "FoodItem");
+        }
+    }
+}
---
+++ CC/CC/Models/Meal.cs
@@ -0,0 +1,32 @@
+﻿using CC.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CC.Models
+{
+    public class Meal
+    {
+        public Meal()
+        {
+            this.carbohydrates = 0;
+            this.calories = 0;
+            this.protein = 0;
+            this.fats = 0;
+        }
+        public string name { get; set; }
+        public float calories { get; set; }
+        public float protein { get; set; }
+        public float carbohydrates { get; set; }
+        public float fats { get; set; }
+
+        public void AddFoodItem(FoodItemConsumed i)
+        {
+            this.calories += i.foodItem.calories * i.quantity / 100;
+            this.protein += i.foodItem.protein * i.quantity / 100;
+            this.carbohydrates += i.foodItem.carbohydrates * i.quantity / 100;
+            this.fats += i.foodItem.fats * i.quantity / 100;
+        }
+    }
+}
